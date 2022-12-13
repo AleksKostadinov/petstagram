0a323459828592f0c8356231af69358cbe24f7d1
AleksKostadinov <kostadinov12@gmail.com>
2022-12-13 02:54:30 +0200

    Filled views and urls for every app to connect with templates

--- petstagram/petstagram/accounts/urls.py
+++ petstagram/petstagram/accounts/urls.py
@@ -1,3 +1,14 @@
+from django.urls import include, path
+
+from petstagram.accounts.views import delete_user, details_user, edit_user, login_user, register_user
+
+
 urlpatterns = [
-    
+    path('login/', login_user, name='login user'),
+    path('register/', register_user, name='register user'),
+    path('profile/<int:pk>/', include([
+        path('', details_user, name='details user'),
+        path('delete/', delete_user, name='delete user'),
+        path('edit/', edit_user, name='edit user'),
+    ])),
 ]
