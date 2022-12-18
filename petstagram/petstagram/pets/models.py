4e0749f1bfa33ad9230d240cabcdfcd33ad55b40
AleksKostadinov <kostadinov12@gmail.com>
2022-12-18 23:21:41 +0200

    Added Slug, Register Pet to admin

--- petstagram/petstagram/pets/models.py
+++ petstagram/petstagram/pets/models.py
@@ -14,4 +14,6 @@ class Pet(models.Model):
     
     name = models.CharField(max_length=MAX_NAME, null=False, blank=False,)
     personal_photo = models.URLField(null=False, blank=False,)
+    slug = models.SlugField(unique= True, null=False, blank=False,)
     date_of_birth = models.DateField(null=True, blank=True,)
+   
