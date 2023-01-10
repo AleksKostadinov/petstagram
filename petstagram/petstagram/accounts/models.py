0d44d39f89916279779ca540ab43346f5a900804
AleksKostadinov <kostadinov12@gmail.com>
2023-01-11 01:47:16 +0200

    Created User. Added to Admin.

--- petstagram/petstagram/accounts/models.py
+++ petstagram/petstagram/accounts/models.py
@@ -1,3 +1,49 @@
+from enum import Enum
+from django.contrib.auth import models as auth_models
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+
+class ChoicesEnumMixin:
+    @classmethod
+    def choices(cls):
+        return [(x.name, x.value) for x in cls]
+        
+    @classmethod
+    def max_len(cls):
+        return max(len(name) for name, _ in cls.choices())
+        
+        
+class Gender(ChoicesEnumMixin, Enum):
+    male = 'Male'
+    female = 'Female'
+    DoNotShow = 'Do not show'
+    
+    
+class AppUser(auth_models.AbstractUser):
+    MIN_LEN_FIRST_NAME = 2
+    MAX_LEN_FIRST_NAME = 30
+    MIN_LEN_LAST_NAME = 2
+    MAX_LEN_LAST_NAME = 30
+
+    first_name = models.CharField(
+        max_length=MAX_LEN_FIRST_NAME,
+        validators=(
+            validators.MinLengthValidator(MIN_LEN_FIRST_NAME),
+        )
+    )
+    last_name = models.CharField(
+        max_length=MAX_LEN_LAST_NAME,
+        validators=(
+            validators.MinLengthValidator(MIN_LEN_LAST_NAME),
+        )
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+    
+    gender = models.CharField(
+        choices=Gender.choices(),
+        max_length=Gender.max_len(),
+    )
