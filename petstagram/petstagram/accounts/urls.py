a8d23858ee04af8e1af1c1700d63963f01305ed6
AleksKostadinov <kostadinov12@gmail.com>
2023-01-12 00:14:52 +0200

    Added SignUpView

--- petstagram/petstagram/accounts/urls.py
+++ petstagram/petstagram/accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import include, path
 
-from petstagram.accounts.views import SignInView, delete_user, details_user, edit_user, register_user
+from petstagram.accounts.views import SignInView, SignUpView, delete_user, details_user, edit_user
 
 
 urlpatterns = [
     path('login/', SignInView.as_view(), name='login user'),
-    path('register/', register_user, name='register user'),
+    path('register/', SignUpView.as_view(), name='register user'),
     path('profile/<int:pk>/', include([
         path('', details_user, name='details user'),
         path('delete/', delete_user, name='delete user'),
--- petstagram/petstagram/accounts/views.py
+++ petstagram/petstagram/accounts/views.py
@@ -1,5 +1,12 @@
-from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+from django.views import generic as views
+from django.contrib.auth import views as auth_views, get_user_model
 from django.shortcuts import render
+
+from petstagram.accounts.forms import UserCreateForm
+
+# Always get the 'user model' with 'get_user_model'
+UserModel = get_user_model()
 
 # def login_user(request):
 #     return render(request, 'accounts/login-page.html')
@@ -8,10 +15,16 @@ class SignInView(auth_views.LoginView):
     template_name = 'accounts/login-page.html'
     
 
-def register_user(request):
-    return render(request, 'accounts/register-page.html')
+# def register_user(request):
+#     return render(request, 'accounts/register-page.html')
 
 
+class SignUpView(views.CreateView):
+    template_name = 'accounts/register-page.html'
+    form_class = UserCreateForm
+    success_url = reverse_lazy('index')
+    
+    
 def delete_user(request, pk):
     return render(request, 'accounts/profile-delete-page.html')
 
