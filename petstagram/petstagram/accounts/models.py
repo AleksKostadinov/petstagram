7dbbe52e0ff71e3242a245e31882bf9e70b23640
AleksKostadinov <kostadinov12@gmail.com>
2023-01-11 02:08:06 +0200

    Added additional validator

--- petstagram/petstagram/accounts/models.py
+++ petstagram/petstagram/accounts/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth import models as auth_models
 from django.db import models
 from django.core import validators
 
+from petstagram.core.validators import validate_only_letters
+
 
 class ChoicesEnumMixin:
     @classmethod
@@ -30,12 +32,14 @@ class AppUser(auth_models.AbstractUser):
         max_length=MAX_LEN_FIRST_NAME,
         validators=(
             validators.MinLengthValidator(MIN_LEN_FIRST_NAME),
+            validate_only_letters,
         )
     )
     last_name = models.CharField(
         max_length=MAX_LEN_LAST_NAME,
         validators=(
             validators.MinLengthValidator(MIN_LEN_LAST_NAME),
+            validate_only_letters,
         )
     )
 
